78235ff0be92ea95d7f4af1e01a618c8f7bb2e5f
Subham Mohanty <121544509+subhamhacks@users.noreply.github.com>
2025-05-18 01:28:21 +0530

    Update Footer.jsx

--- clients/src/components/Footer.jsx
+++ clients/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import logo from "../img/logo.png";
 const Footer = () => {
     return (
         <footer>
-            <img src="https://github.com/subhamhacks/Full-Stack-Blog-App/blob/main/clients/src/img/bloglogo.png" alt="Blog by Annetta"/>
+            <img src="/bloglogo.png" alt="Blog by Annetta"/>
             <span>
                 Website created by Subham with React.js.<br />
                 Contact: mohanty.subham07@gmail.com
