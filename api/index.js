f957a98f8caf8041c0e9f0559694d53ac4d26589
Subham Mohanty <121544509+subhamhacks@users.noreply.github.com>
2025-05-18 09:03:59 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js";
+import postRoutes from "./routes/post.js";
+import cookieParser from "cookie-parser";
+import multer from "multer";
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
